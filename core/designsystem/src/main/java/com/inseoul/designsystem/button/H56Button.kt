2487c236ca876903bba62e56e9cb6febee48b332
treemiddle <wj122791@gmail.com>
2022-12-07 00:29:44 +0900

    H56 Button UI 추가

---
+++ core/designsystem/src/main/java/com/inseoul/designsystem/button/H56Button.kt
@@ -0,0 +1,29 @@
+package com.inseoul.designsystem.button
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun H56Button(
+    modifier: Modifier = Modifier,
+    title: String,
+    cornerRadius: Int,
+    backgroundColor: Color,
+    onClicked: () -> Unit
+) {
+    Button(
+        modifier = modifier.height(height = 56.dp),
+        shape = RoundedCornerShape(size = cornerRadius.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = backgroundColor),
+        onClick = { onClicked() },
+    ) {
+        Text(text = title)
+    }
+}
