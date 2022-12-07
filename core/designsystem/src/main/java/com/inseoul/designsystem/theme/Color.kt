6209379cc7a52ba649c83d472c5ff1e67c96ce4f
DavidKwon7 <khjunejune10@gmail.com>
2022-12-07 14:18:04 +0900

    Color.kt 추가

---
+++ core/designsystem/src/main/java/com/inseoul/designsystem/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.inseoul.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val gray00 = Color(0xFFFFFFFF)
+internal val gray50 = Color(0xFFF7F8FA)
+internal val gray100 = Color(0xFFF2F3F6)
+internal val gray200 = Color(0xFFE6E7EB)
+internal val gray300 = Color(0xFFDCDEE3)
+internal val gray400 = Color(0xFFD1D3D8)
+internal val gray500 = Color(0xFFADB1BA)
+internal val gray600 = Color(0xFF868B94)
+internal val gray700 = Color(0xFF4D5159)
+internal val gray800 = Color(0xFF393A40)
+internal val gray900 = Color(0xFF212124)
+
+internal val Green50 = Color(0xFFDBDCD8)
+internal val Green100 = Color(0xFFB7C4B5)
+internal val Green200 = Color(0xFF91A68F)
+internal val Green300 = Color(0xFF52674F)
