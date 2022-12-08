ba9d5aaec76174c6b3306095d5d1b2d25fd43044
DavidKwon7 <khjunejune10@gmail.com>
2022-12-08 15:00:54 +0900

    아이콘 정리를 위한 InseoulIcons.kt 생성

---
+++ core/designsystem/src/main/java/com/inseoul/designsystem/icon/InseoulIcons.kt
@@ -0,0 +1,22 @@
+package com.inseoul.designsystem.icon
+
+import com.inseoul.designsystem.R
+
+object InseoulIcons {
+    val ArrowBack = R.drawable.icon_arrow_back
+    val ArrowDecreaseMono = R.drawable.icon_arrow_decrease_mono
+    val ArrowIncreaseMono = R.drawable.icon_arrow_increase_mono
+    val ArrowLeft = R.drawable.icon_arrow_left
+    val ArrowRight = R.drawable.icon_arrow_right
+    val Bell = R.drawable.icon_bell
+    val CalendarCheckMono = R.drawable.icon_calendar_check_mono
+    val CheckFill = R.drawable.icon_check_fill
+    val ClockMono = R.drawable.icon_clock_mono
+    val PencilMono = R.drawable.icon_pencil_mono
+    val Pets = R.drawable.icon_pets
+    val PlayMono = R.drawable.icon_play_mono
+    val SettingMono = R.drawable.icon_setting_mono
+    val StarMono = R.drawable.icon_star_mono
+    val UserMono = R.drawable.icon_user_mono
+    val XCircleMono = R.drawable.icon_x_circle_mono
+}
