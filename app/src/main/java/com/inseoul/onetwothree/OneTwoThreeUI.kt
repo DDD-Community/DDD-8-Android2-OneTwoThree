69ec395e39f0daf5f780c143cb566cf6835a8f9f
DavidKwon7 <khjunejune10@gmail.com>
2022-12-11 16:36:53 +0900

    Navigation 적용

---
+++ app/src/main/java/com/inseoul/onetwothree/OneTwoThreeUI.kt
@@ -0,0 +1,46 @@
+package com.inseoul.onetwothree
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.inseoul.onetwothree.navigation.InseoulNavGraph
+import com.inseoul.onetwothree.ui.theme.OneTwoThreeTheme
+
+@Composable
+fun OneTwoThreeUI() {
+    OneTwoThreeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            InseoulNavGraph()
+        }
+    }
+}
+
+// 테스트를 위해 생성
+@Composable
+fun ScreenOne(navController: NavHostController) {
+    Column {
+        Text(text = "첫번째 페이지 입니다.")
+        Button(onClick = { navController.navigate("screen_two") }) {
+            Text(text = "다음")
+        }
+    }
+}
+
+@Composable
+fun ScreenTwo(navController: NavHostController) {
+    Column {
+        Text(text = "두번째 페이지 입니다")
+        Button(onClick = { navController.navigate("screen_one") }) {
+            Text(text = "이전")
+        }
+    }
+}
--- app/src/main/java/com/inseoul/onetwothree/navigation/InseoulNavGraph.kt
+++ app/src/main/java/com/inseoul/onetwothree/navigation/InseoulNavGraph.kt
@@ -1,43 +1,19 @@
 package com.inseoul.onetwothree.navigation
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.inseoul.onetwothree.ScreenOne
+import com.inseoul.onetwothree.ScreenTwo
 
 @Composable
-fun InseoulNavGraph(
-    modifier: Modifier,
-) {
+fun InseoulNavGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "screen_one") {
-        composable("users_list") { ScreenOne(navController) }
-        composable("user_detail") { ScreenTwo(navController) }
-    }
-}
-// 테스트를 위해 생성
-@Composable
-fun ScreenOne(navController: NavHostController) {
-    Column {
-        Text(text = "Hello!")
-        Button(onClick = { navController.navigate("screen_one") }) {
-
-        }
+        composable("screen_one") { ScreenOne(navController) }
+        composable("screen_two") { ScreenTwo(navController) }
     }
 }
 
-@Composable
-fun ScreenTwo(navController: NavHostController) {
-    Column {
-        Text(text = "detail screnn")
-        Button(onClick = { navController.navigate("screen_two") }) {
-
-        }
-    }
-}
