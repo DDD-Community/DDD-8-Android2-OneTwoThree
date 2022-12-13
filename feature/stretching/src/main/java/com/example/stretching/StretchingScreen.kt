83d6671dfa3f797ad164d5771840721b9665f5e9
DavidKwon7 <khjunejune10@gmail.com>
2022-12-13 20:56:21 +0900

    TimerProgressBar UI 추가

--- feature/stretching/src/main/java/com/example/stretching/StretchingScreen.kt
+++ feature/stretching/src/main/java/com/example/stretching/StretchingScreen.kt
@@ -1,16 +1,7 @@
 package com.example.stretching
 
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-
-/*
-designsystem 모듈이 추가되면, 추가 후 작업..
-1. 타임 바를 비롯한 표면 ui 작업
-2. 뒤로 가기 버튼, dialog 띄워주기
-3. 타임 완료 시, 추가 기능 정리
-4. 완료 화면 구현
-*/
 
 @Composable
 fun StretchingScreen() {
@@ -20,5 +11,6 @@ fun StretchingScreen() {
 @Preview
 @Composable
 fun StretchingPreview() {
-    StretchingScreen()
+    //StretchingScreen()
+    Timer()
 }
