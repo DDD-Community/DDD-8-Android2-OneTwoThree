c0d9ae2dd3ce740b86824ad2502605da88ed6036
treemiddle <wj122791@gmail.com>
2022-12-17 15:06:29 +0900

    SettingsScreen.kt 추가

---
+++ feature/setting/src/main/java/com/treemiddle/setting/screen/SettingsScreen.kt
@@ -0,0 +1,110 @@
+package com.treemiddle.setting.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SettingsScreen() {
+    Surface {
+        Column(modifier = Modifier.fillMaxSize()) {
+            SettingsToolbar()
+            SettingsMenu(
+                menuDrawableImageVector = Icons.Default.AccountCircle,
+                menuTitle = "고양이 이름 변경하기"
+            )
+            SettingsMenu(
+                menuDrawableImageVector = Icons.Default.Menu,
+                menuTitle = "알림"
+            )
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp)
+                    .background(Color.Blue)
+                    .height(1.dp)
+            )
+            SettingsAppVersion(appVersion = "1.0")
+        }
+    }
+}
+
+// NOTE : InseoulToolbar를 사용해야 합니다.
+@Composable
+fun SettingsToolbar() {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Red)
+            .padding(bottom = 18.dp)
+            .height(56.dp),
+        text = "툴바가 들어가야합니다.",
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun SettingsMenu(
+    modifier: Modifier = Modifier,
+    menuDrawableImageVector: ImageVector, // NOTE : 임시로 ImageVector로 설정합니다.
+    menuTitle: String
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(58.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(start = 18.dp)
+                .size(24.dp),
+            imageVector = menuDrawableImageVector,
+            contentDescription = null
+        )
+        Text(
+            modifier = Modifier
+                .padding(start = 18.dp)
+                .weight(1f),
+            text = menuTitle,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Image(
+            modifier = Modifier.padding(end = 16.dp),
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+fun SettingsAppVersion(appVersion: String) {
+    Column(modifier = Modifier.padding(start = 16.dp, top = 24.dp)) {
+        Text(
+            text = "앱 버전",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp
+        )
+        Text(
+            text = appVersion,
+            fontSize = 14.sp
+        )
+    }
+}
