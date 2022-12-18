46c34cd528a78824218a524229b58091cddcc1e4
treemiddle <wj122791@gmail.com>
2022-12-19 00:37:11 +0900

    libarary-module에서 CalendarDay.kt 수정

--- library-calendar/src/main/java/com/inseoul/library_calendar/CalendarDay.kt
+++ library-calendar/src/main/java/com/inseoul/library_calendar/CalendarDay.kt
@@ -1,32 +1,48 @@
 package com.inseoul.library_calendar
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.inseoul.designsystem.theme.gray400
+import com.inseoul.designsystem.theme.gray900
 
-/*NOTE
-디자인 시스템 모듈에 있는 컬러값을 사용하기 위해
-해당 모듈에서 core-designsystem 모듈을 추가하면 빌드 에러나오는데 원인을 모르겠음*/
 @Composable
 fun CalendarDay(
     day: String,
     isActivated: Boolean,
-    onDayClicked: () -> Unit
+    isClicked: Boolean,
+    onDayClicked: (Boolean, Boolean, Int) -> Unit,
 ) {
+    val modifier = Modifier
+        .size(size = 40.dp)
+        .padding(2.dp)
+        .clickable {
+            onDayClicked(isActivated, isClicked, day.toInt())
+        }
+    val currentDayModifier = if (isClicked) {
+        modifier.border(
+            width = 1.5.dp,
+            color = gray900,
+            shape = CircleShape
+        )
+    } else {
+        modifier
+    }
+
     Text(
-        modifier = Modifier
-            .size(size = 40.dp)
-            .clickable { onDayClicked() },
+        modifier = currentDayModifier,
         text = day,
         color = if (isActivated) {
-            Color(0xFF212124)
+            gray900
         } else {
-            Color(0xFFD1D3D8)
+            gray400
         },
         textAlign = TextAlign.Center
     )
