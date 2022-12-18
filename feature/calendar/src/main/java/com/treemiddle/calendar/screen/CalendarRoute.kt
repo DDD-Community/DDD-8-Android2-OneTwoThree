01effe8055cc850adcb26a0c70447ecfe125ab73
treemiddle <wj122791@gmail.com>
2022-12-18 17:22:52 +0900

    CalendarRoute.kt 에서 viewmodel property hosting

--- feature/calendar/src/main/java/com/treemiddle/calendar/screen/CalendarRoute.kt
+++ feature/calendar/src/main/java/com/treemiddle/calendar/screen/CalendarRoute.kt
@@ -1,14 +1,29 @@
 package com.treemiddle.calendar.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.inseoul.library_calendar.CalendarViewModel
 
+@OptIn(ExperimentalLifecycleComposeApi::class)
 @Composable
 fun CalendarRoute(
     modifier: Modifier = Modifier,
     viewModel: CalendarViewModel = hiltViewModel()
 ) {
-    CalendarScreen(viewModel = viewModel)
+    val dayStateList by viewModel.calendarState.collectAsStateWithLifecycle()
+    val calendarTitle by viewModel.calendarTitle.collectAsStateWithLifecycle()
+
+     CalendarScreen(
+         dayStateList = dayStateList,
+         currentTitle = calendarTitle,
+         onBackButtonClicked = {
+             // 이전 화면으로 이동해야 합니다.
+         },
+         onPreviousClicked =  { viewModel.onPreviousClick() },
+         onNextClicked = { viewModel.onNextClick() }
+     )
 }
