68ec2ba2ce35a15ea97523f90f37728437dfc26f
DavidKwon7 <khjunejune10@gmail.com>
2022-12-21 22:55:04 +0900

    DropdownMenu 디자인 수정

--- core/designsystem/src/main/java/com/inseoul/designsystem/dropdownmenu/InseoulDropDownMenu.kt
+++ core/designsystem/src/main/java/com/inseoul/designsystem/dropdownmenu/InseoulDropDownMenu.kt
@@ -2,18 +2,16 @@ package com.inseoul.designsystem.dropdownmenu
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.DropdownMenu
-import androidx.compose.material.DropdownMenuItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.inseoul.designsystem.icon.InseoulIcons
 
 @Composable
 fun InSeoulDropDownMenu(list: List<String>) {
@@ -22,35 +20,54 @@ fun InSeoulDropDownMenu(list: List<String>) {
     val currentValue = remember { mutableStateOf(list[0]) }
 
     val icon = if (expanded.value)
-        Icons.Filled.KeyboardArrowUp
+        InseoulIcons.ArrowIncreaseMono
     else
-        Icons.Filled.KeyboardArrowDown
+        InseoulIcons.ArrowDecreaseMono
 
     Box(modifier = Modifier.fillMaxWidth()) {
-        Row(
-            modifier = Modifier
-                .height(24.dp)
-                .width(154.dp)
-                .clickable {
-                    expanded.value = !expanded.value
-                }) {
-            Text(text = currentValue.value)
-            Icon(imageVector = icon, contentDescription = "DropDownIcon")
+        Column() {
+            Row(
+                modifier = Modifier
+                    .width(154.dp)
+                    .height(24.dp)
+                    .clickable {
+                        expanded.value = !expanded.value
+                    }) {
 
+                Text(
+                    modifier = Modifier
+                        .weight(1f)
+                        .width(122.dp),
+                    text = currentValue.value, fontSize = 18.sp
+                )
 
-            DropdownMenu(expanded = expanded.value,
-                onDismissRequest = {
-                    expanded.value = false
-                }) {
-                list.forEach {
-                    DropdownMenuItem(onClick = {
-                        currentValue.value = it
+                Icon(
+                    painter = painterResource(id = icon),
+                    contentDescription = "DropDownIcon",
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+
+                DropdownMenu(expanded = expanded.value,
+                    onDismissRequest = {
                         expanded.value = false
                     }) {
-                        Text(text = it)
+                    list.forEach {
+                        DropdownMenuItem(onClick = {
+                            currentValue.value = it
+                            expanded.value = false
+                        }) {
+                            Text(text = it)
+                        }
                     }
                 }
             }
+            Spacer(modifier = Modifier.height(8.dp))
+            Divider(
+                color = Color.Black,
+                modifier = Modifier
+                    .height(2.dp)
+                    .width(154.dp)
+            )
         }
     }
 }
