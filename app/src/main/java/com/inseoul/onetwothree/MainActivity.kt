41c13370b9eb5cc70675d5b7458e62c3e2abe4c9
DavidKwon7 <khjunejune10@gmail.com>
2023-01-07 15:07:28 +0900

    FCM 기능 추가

--- app/src/main/java/com/inseoul/onetwothree/MainActivity.kt
+++ app/src/main/java/com/inseoul/onetwothree/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.inseoul.onetwothree
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -11,6 +14,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OneTwoThreeUI()
+            getFCMToken()
         }
+    }
+
+    private fun getFCMToken(): String? {
+        var token: String? = null
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            token = task.result
+
+            // Log and toast
+            Log.d(TAG, "FCM Token is ${token}")
+        })
+
+        return token
+    }
+
+    companion object {
+        val TAG = "MainActivity.class"
     }
 }
