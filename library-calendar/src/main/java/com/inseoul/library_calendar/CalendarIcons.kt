9fe2ab8a4d75526648a1b780cafac913badb1f97
treemiddle <wj122791@gmail.com>
2023-01-14 12:38:21 +0900

    캘린더 이미지 추가

---
+++ library-calendar/src/main/java/com/inseoul/library_calendar/CalendarIcons.kt
@@ -0,0 +1,6 @@
+package com.inseoul.library_calendar
+
+object CalendarIcons {
+    val stretchTotalCountUnderFive = R.drawable.stretch_disabled
+    val stretchTotalCountMoreFive = R.drawable.stretch_activated
+}
