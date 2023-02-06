9bf705eef320e5ce473fee778661e5719fe8465f
DavidKwon7 <khjunejune10@gmail.com>
2023-02-06 12:47:14 +0900

    AlarmSettingViewModel에서 List is empty 에러 해결

--- feature/alarmsetting/src/main/java/com/inseoul/alarmsetting/AlarmSettingViewModel.kt
+++ feature/alarmsetting/src/main/java/com/inseoul/alarmsetting/AlarmSettingViewModel.kt
@@ -4,13 +4,23 @@ import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
-class AlarmSettingViewModel: ViewModel() {
+class AlarmSettingViewModel : ViewModel() {
 
     private val _alarmDay = mutableStateListOf<String>()
     val alarmDay: List<String> = _alarmDay
 
     fun addAlarmDay(day: String) {
-        _alarmDay.add(day)
+        // _alarmDay.add(day)
+
+        if (_alarmDay.contains(day)) {
+            _alarmDay.remove(day)
+        } else {
+            _alarmDay.add(day)
+        }
+        if (_alarmDay.isNullOrEmpty()) {
+            Log.d("TAG", "addAlarmDay: 결과를 입력해주세요")
+            _alarmDay.add("")
+        }
 
         Log.d("AlarmSettingVM", "addAlarmDay: ${_alarmDay.last()}")
     }
